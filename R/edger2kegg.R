2580617df146b56d8b0cd52191d96a5dba3b456e
kek12e <katiekyle93@gmail.com>
2016-09-29 11:55:37 -0400

    parallelize loop through pathways for speed

--- R/edger2kegg.R
+++ R/edger2kegg.R
@@ -1,7 +1,7 @@
 # param edgerfiles - vector of edger filepaths 
 # param pathways - if not 'all', character vector of pathway ids from http://www.genome.jp/kegg/pathway.html
 # param sigonly - if TRUE, only color significantly differentially expressed items, else color all 
-edger2kegg <- function( edgerfiles , organism="hsa" , pathways="all" , limits=c(-1,1) , entrezOrganism="Hs", sigonly=FALSE, pval=FALSE ){
+edger2kegg <- function( edgerfiles , organism="hsa" , pathways="all" , limits=c(-1,1) , entrezOrganism="Hs", sigonly=FALSE, pval=FALSE, threads=getOption("threads",1L) ){
 
   library(pathview)
   library(KEGGREST)
@@ -14,6 +14,7 @@ edger2kegg <- function( edgerfiles , organism="hsa" , pathways="all" , limits=c(
   de=tsvRead(edgerfiles,col_names=T)
 
   # each edger file is a separate comparision, loop through each file 
+  #dump <- mclapply(1:length(edgerfile),function(i) {
   for(i in 1:length(edgerfiles)) {
 
     # convert gene symbols to entrez IDs
@@ -81,7 +82,8 @@ edger2kegg <- function( edgerfiles , organism="hsa" , pathways="all" , limits=c(
         vals[which(vals[,"PValue"]>0.05),1] <- 0   # set logFC to 0 for all insignificant items (based on PValue)
     }
 
-    for(j in 1:numdbs){
+    dump <- mclapply(1:numdbs, function(j) {
+    #for(j in 1:numdbs){
       cat(removeext(edgerfiles[i]),": ",dbnames[j],"\n")
       res1 = tryCatch({
         pathview( gene.data=vals[,1],          pathway.id=as.character(dbid[j]),species=organism,out.suffix=paste0(dbshortnames[j],"_",removeext(edgerfiles[i]), "_log2ratio_pathview"              ), sign.pos="bottomleft", kegg.native=FALSE, limit=list(cpd=limits,gene=limits),low =list(gene = "red", cpd = "yellow") , mid = list(gene = "gray", cpd= "gray"), high =list(gene = "green", cpd = "blue") )
@@ -102,9 +104,9 @@ edger2kegg <- function( edgerfiles , organism="hsa" , pathways="all" , limits=c(
           cat("\tpathview() call #2 done.\n")
       })
       
-    } # \for each db
+    }, mc.cores=threads,mc.preschedule=F) #} # \for each db
         
-  } # \for each file in edgerfiles
+  } # \for each file in edgerfiles # }, mc.cores=threads,mc.preschedule=F)) 
 
 } # \edger2kegg()
 
