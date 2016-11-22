f223e9a69e93d6235592046c8da52b7306dda35a
Daniel Vera <dvera@bio.fsu.edu>
2016-11-22 15:56:53 -0500

    various

---
+++ R/bedpeToBed12.R
@@ -0,0 +1,16 @@
+bedpeToBed12 <- function( bedpeFiles , threads=getOption("threads",1L) ){
+
+  outnames <- paste0(basename(removeext(bedpeFiles)),".bed12")
+
+  cmdString <- paste(
+    "awk '{if($1==$4 && $2<$5){",
+      "print $1,$2,$3,$7,0,\"+\",$2,$6,$8,2,$3-$2\",\"$6-$5,0\",\"$5-$2",
+    "} else if ($1==$4 && $2>$5){",
+      "print $1,$5,$6,$7,0,\"+\",$6,$2,$8,2,$6-$5\",\"$3-$2,0\",\"$2-$5",
+    "}}' OFS='\t' ",bedpeFiles, "> ", outnames
+  )
+
+  res <- cmdRun(cmdString,threads=threads)
+
+  return(outnames)
+}
