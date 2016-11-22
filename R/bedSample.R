59c4d0bc296ce8a138a11d5042767f5d95e73715
Daniel Vera <dvera@bio.fsu.edu>
2016-11-22 15:56:14 -0500

    various

--- R/bedSample.R
+++ R/bedSample.R
@@ -1,9 +1,10 @@
-bedSample <-
-function( beds , count , threads="max" ){
+bedSample <-function( beds , count , threads=getOption(threads,1L) ){
 	library(tools)
-	ext<-file(ext(bed))
+	ext<-file_ext(beds)
 	#totallines<-filelines(bed)
 	#if(count > totallines ){stop("sample quantity is greater than available lines")}
-	outname<-paste0(basename(removeext(bed)),"_random",count,".",ext)
-	system(paste("shuf -n",count,beds,">",newname))
+	outname<-paste0(basename(removeext(beds)),"_random",count,".",ext)
+	cmdString=paste("shuf -n",count,beds,">",outname)
+	res <- cmdRun(cmdString,threads=threads)
+	return(outname)
 }
