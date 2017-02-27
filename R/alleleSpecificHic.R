8db9b46c25304bad92ba4e48694cd6aba58ad34c
Daniel Vera <dvera@bio.fsu.edu>
2017-02-27 10:25:07 -0500

    updateAlleleSpecificHic

--- R/alleleSpecificHic.R
+++ R/alleleSpecificHic.R
@@ -6,15 +6,30 @@ alleleSpecificHic <- function( fastqFiles1 , fastqFiles2=NULL , index1prefix, in
   sam_g2r1 <- bowtie2(fastqFiles1, index2prefix, discordant=TRUE, appendIndexToName=TRUE, reorder=TRUE, threads=threads )
   sam_g2r2 <- bowtie2(fastqFiles2, index2prefix, discordant=TRUE, appendIndexToName=TRUE, reorder=TRUE, threads=threads )
 
+  fname1 <- basename(removeext(fastqFiles1))
+  fname2 <- basename(removeext(fastqFiles2))
 
   fields=c("AS","XM","XO","XG","NM")
 
-  pre1 <- paste0(removeext(sam_g1r1),"_preparsed.sam")
-  pre2 <- paste0(removeext(sam_g1r2),"_preparsed.sam")
+  i1p <- basename(index1prefix)
+  i2p <- basename(index2prefix)
 
-  sys1 <- paste0(removeext(sam_g1r1),"_preparsed.sh")
-  sys2 <- paste0(removeext(sam_g1r2),"_preparsed.sh")
+  pre1 <- paste0(fname1,"_genflagged.sam")
+  pre2 <- paste0(fname2,"_genflagged.sam")
 
+  sys1 <- paste0(fname1,"_parse.sh")
+  sys2 <- paste0(fname2,"_parse.sh")
+
+
+  par11 <- paste0(fname1,"_",i1p,"-",i1p,".sam")
+  par22 <- paste0(fname1,"_",i2p,"-",i2p,".sam")
+  par12 <- paste0(fname1,"_",i1p,"-",i2p,".sam")
+  par13 <- paste0(fname1,"_",i1p,"-amb.sam")
+  par23 <- paste0(fname1,"_",i2p,"-amb.sam")
+  par01 <- paste0(fname1,"_",i1p,"-unm.sam")
+  par02 <- paste0(fname1,"_",i2p,"-unm.sam")
+  par00 <- paste0(fname1,"_unm-unm.sam")
+  par33 <- paste0(fname1,"_amb-amb.sam")
 
 
 
@@ -83,37 +98,44 @@ alleleSpecificHic <- function( fastqFiles1 , fastqFiles2=NULL , index1prefix, in
   if(threads2<1){threads2<-1}
   cmdRun(cmds,threads=threads2)
 
-  res <- cmdRun(cmdString,threads=threads)
+  # res <- cmdRun(cmdString,threads=threads)
 
-
-  cmdString <- paste(
-    "awk '{if($1~\"@\"){print $0} else{exit 0}}' OFS='\t'",allsams[r1],">", genparsed[r1]
+  cmdString <- paste0(
+    "paste ",pre1," ",pre2," | awk '{",
+      "if($1~\"@\"){",
+        paste("print $1,$2,$3 > \"",par11,"\";print $1,$2,$3 > \"",par22,"\";print $1,$2,$3 > \"",par33,"\";print $1,$2,$3 > \"",par00,"\";print $1,$2,$3 > \"",par12,"\";print $1,$2,$3 > \"",par23,"\";print $1,$2,$3 > \"",par01,"\";print $1,$2,$3 > \"",par02,"\";print $1,$2,$3 > \"",par13,"\""),"}",
+      "else if($",nf+1,"==\"PG:i:1\" && $",(nf*2)+2,"==\"PG:i:1\"){",
+        "P11++; print ",paste0("$",1:nf,collapse=",")," > \"",par11,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par11,"\"}",
+      "else if($",nf+1,"==\"PG:i:2\" && $",(nf*2)+2,"==\"PG:i:2\"){",
+          "P22++; print ",paste0("$",1:nf,collapse=",")," > \"",par22,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par22,"\"}",
+      "else if($",nf+1,"==\"PG:i:3\" && $",(nf*2)+2,"==\"PG:i:3\"){",
+          "P33++; print ",paste0("$",1:nf,collapse=",")," > \"",par33,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par33,"\"}",
+      "else if($",nf+1,"==\"PG:i:0\" && $",(nf*2)+2,"==\"PG:i:0\"){",
+        "P00++; print ",paste0("$",1:nf,collapse=",")," > \"",par00,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par00,"\"}",
+      "else if( ($",nf+1,"==\"PG:i:1\" && $",(nf*2)+2,"==\"PG:i:3\") || ($",nf+1,"==\"PG:i:3\" && $",(nf*2)+2,"==\"PG:i:1\")){",
+        "P13++; print ",paste0("$",1:nf,collapse=",")," > \"",par13,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par13,"\"}",
+      "else if( ($",nf+1,"==\"PG:i:2\" && $",(nf*2)+2,"==\"PG:i:3\") || ($",nf+1,"==\"PG:i:3\" && $",(nf*2)+2,"==\"PG:i:2\")){",
+        "P23++; print ",paste0("$",1:nf,collapse=",")," > \"",par23,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par23,"\"}",
+      "else if( ($",nf+1,"==\"PG:i:1\" && $",(nf*2)+2,"==\"PG:i:2\") || ($",nf+1,"==\"PG:i:2\" && $",(nf*2)+2,"==\"PG:i:1\")){",
+        "P12++; print ",paste0("$",1:nf,collapse=",")," > \"",par12,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par12,"\"}",
+      "else if( ($",nf+1,"==\"PG:i:0\" && $",(nf*2)+2,"==\"PG:i:1\") || ($",nf+1,"==\"PG:i:1\" && $",(nf*2)+2,"==\"PG:i:0\")){",
+        "P01++; print ",paste0("$",1:nf,collapse=",")," > \"",par01,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par01,"\"}",
+      "else if( ($",nf+1,"==\"PG:i:0\" && $",(nf*2)+2,"==\"PG:i:2\") || ($",nf+1,"==\"PG:i:2\" && $",(nf*2)+2,"==\"PG:i:0\")){",
+        "P02++; print ",paste0("$",1:nf,collapse=",")," > \"",par02,"\"; print ",paste0("$",(nf+2):((2*nf)+2),collapse=",")," > \"",par02,"\"}",
+    "} END{",
+      "print \"total mate pairs processed:\",NR;",
+      "print \"P11:\",P11,100*P11/NR;",
+      "print \"P22:\",P22,100*P22/NR;",
+      "print \"P12:\",P12,100*P12/NR;",
+      "print \"P13:\",P13,100*P13/NR;",
+      "print \"P23:\",P23,100*P23/NR;",
+      "print \"P01:\",P01,100*P01/NR;",
+      "print \"P02:\",P02,100*P02/NR;",
+      "print \"P00:\",P00,100*P00/NR;",
+      "print \"P33:\",P33,100*P33/NR",
+    "}' OFS='\t'"
   )
-  res <- cmdRun(cmdString,threads=threads)
 
-  if(unequal | paired){
 
-    # if genome-specific sams don't exist, make them so merging doesn't fail.
-    if(unequal){
-      noe <- which(!file.exists(spcparsed))
-      if(length(noe)>1){file.create(spcparsed[noe])}
-    }
-
-    cat("consolidating parsed reads\n")
-    cmdString <- paste(
-      "sort -k1,1 -m",
-      preparsed[r1],
-      if(paired){preparsed[r2]},
-      if(unequal){spcparsed[r1]},
-      if(paired & unequal){spcparsed[r2]},
-      "| awk '{if($1!=p){print $0}; p=$1}' OFS='\t' >> ",genparsed[r1]
-    )
-    res <- cmdRun(cmdString,threads=threads)
-  } else{
-    cmdString <- paste("cat",preparsed[r1],">>",genparsed[r1])
-    res <- cmdRun(cmdString, threads=threads)
-  }
-
-  return(genparsed[r1])
 
 }
