84aee3816d7b17dae902ebee19a1901c2c220028
Daniel Vera <vera@genomics.fsu.edu>
2017-03-26 09:26:37 -0400

    Update bgSort.R

--- R/bgSort.R
+++ R/bgSort.R
@@ -2,7 +2,7 @@ bgSort <- function( bgfiles, threads=getOption("threads",1L), decreasing=FALSE )
 
 	ext<-file_ext(bgfiles)
 	outnames<-paste0(basename(removeext(bgfiles)),"_scoresort.tmp")
-	cmdString <- paste0("sort -g -T . -k4,4n",if(decreasing){"r"}," ",bgfiles," -o ",outnames)
+	cmdString <- paste0("sort -T . -k4,4g",if(decreasing){"r"}," ",bgfiles," -o ",outnames)
 	res <- cmdRun(cmdString,threads=threads)
 	return(outnames)
 
