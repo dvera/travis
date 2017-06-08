a48d6fd3ef9518432214939186bc222df98facad
Daniel Vera <vera@genomics.fsu.edu>
2017-06-07 21:24:11 -0400

    Create bgAcf.R

--- R/bgAcf.R
+++ R/bgAcf.R
@@ -3,7 +3,7 @@ bgAcf <- function ( bgs , maxlag=10, threads=getOption("threads",1L) ){
 
   numbgs=length(bgs)
   bgl <- bgRead(bgs,threads=threads)
-  acfmat = t(as.data.frame( lapply( lapply( bgl, acf, lag.max=maxlag, plot=F), "[[", 1 ) ))
+  acfmat = t(as.data.frame( lapply( mclapply( bgl, acf, lag.max=maxlag, plot=F,mc.cores=threads,mc.preschedule=FALSE), "[[", 1 ) ))
   #a=-1*data.matrix(as.data.frame((lapply(a,diff))))
 
   acfmat=acfmat[,-1,drop=F]
