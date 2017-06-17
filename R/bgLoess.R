59d96f70a7385deaea90d37148f6985f6948c890
Daniel Vera <vera@genomics.fsu.edu>
2017-06-17 08:03:09 -0400

    Update bgLoess.R

--- R/bgLoess.R
+++ R/bgLoess.R
@@ -1,87 +1,60 @@
 bgLoess <-
-function( bgfiles, lspan=0.05, threads=getOption("threads",1L), omitY=FALSE, omitM=FALSE, mbg=FALSE){
+function( bgfiles, lspan, threads=getOption("threads",1L), mbg=FALSE ){
 
+	options(scipen=9999)
 	# assumes sorted bg
-
+	if(lspan<1){
+		stop("lspan must indicate distance in bp")
+	}
 	bgnames <- basename(removeext(bgfiles))
 	numbgs <- length(bgfiles)
-	outnames <- paste(bgnames,"_loess",gsub("\\.","-",lspan),".bg",sep="")
+	outnames <- paste(bgnames,"_loess",lspan,".bg",sep="")
 	chromthreads <- floor(threads/numbgs)
 	if(chromthreads==0){chromthreads=1}
 
-	dump <- mclapply(seq_len(numbgs), function(x){
-		curbg <- read_tsv ( bgfiles[x], col_names=mbg )
-		if(mbg){columnnames=colnames(curbg)}
-
-		if(omitY){curbg=curbg[which(curbg[,1]!="chrY"),]}
-		if(omitM){curbg=curbg[which(curbg[,1]!="chrM"),]}
-
-		#curbg$V4[is.infinite(curbg$V4)]<-NA
+	#dump <- mclapply(seq_len(numbgs), function(x){
+	dump <- lapply(seq_len(numbgs), function(x){
+		curbg <- as.data.frame( read.table ( bgfiles[x], header=mbg, stringsAsFactors=FALSE ) )
+		
+		if(mbg){
+			columnnames=colnames(curbg)
+		}
 
 		chroms    <- unique(curbg[,1])
 		numchroms <- length(chroms)
-
-		pointsperchrom <- as.numeric(table(curbg[,1]))
-		pointratios    <-	max(pointsperchrom)/pointsperchrom
-		scaledspans    <- lspan*pointratios
-
-		smoothstats <- data.frame( chr=chroms , numPoints=pointsperchrom , scaledSpans=scaledspans)
-
-		if(getOption("verbose")){print(paste("smoothing stats for",bgnames[x]));print(smoothstats)}
+		windowsize <- median(curbg[,3]-curbg[,2])
 		all=split(curbg,curbg[,1])
-
-		lscores<-mclapply(1:numchroms,function(i){
+		
+		#lscores<-mclapply(1:numchroms,function(i){
+		lscores<-lapply(1:numchroms,function(i){
+			
 			cur <- all[[i]]
-
-			cura <- lapply(4:ncol(cur), function(k){
-				cur[,k] <- tryCatch(
-					{
-						loess(cur[,k]~cur[,2],span=scaledspans[i])$fitted
-
-					},
-					warning = function(war){
+			chromlspan <- lspan/sum(cur[,3]-cur[,2])
+			
+			cura <- as.data.frame(lapply(4:ncol(cur), function(k){
+				cur[,k] <- tryCatch({
+						loess(cur[,k]~cur[,2],span=chromlspan)$fitted
+					},warning = function(war){
 						print(paste("warning for file",bgnames[x],"chromosome",cur[1,1],":",war))
-						out <- loess(cur[,k]~cur[,2],span=scaledspans[i])$fitted
+						out <- loess(cur[,k]~cur[,2],span=chromlspan)$fitted
 						return(out)
-					},
-					error = function(err){
+					},error = function(err){
 						print(paste("smoothing failed for file",bgnames[x],"chromosome",cur[1,1],":",err))
 						return(cur[,k])
 					}
 				)
-			})
-			cura <- as.data.frame(cura)
-			colnames(cura) <- columnnames[-(1:3)]
+			}))
+			
 			cura <- cbind(cur[,1:3],cura)
-			#print(str(cura))
-
+			colnames(cura) <- colnames(cur)
 			return(cura)
-		},mc.cores=chromthreads, mc.preschedule=FALSE)
-		print(str(lscores))
-		curbg<-do.call(rbind,lscores)
-		tsvWrite(curbg,file=outnames[x],col_names=mbg)
+		#},mc.cores=chromthreads, mc.preschedule=FALSE)
+		})
+		smoothbg<-do.call(rbind,lscores)
+		write.table(smoothbg,file=outnames[x],col.names=mbg,quote=FALSE, sep="\t", row.names=F)
 		rm(curbg)
 		gc()
-
-	}, mc.cores=threads, mc.preschedule=FALSE)
-
+	#}, mc.cores=threads, mc.preschedule=FALSE)
+	})
 	return(outnames)
-
-	#chroms<-mixedsort(chroms)
-	#cat(bgname,": reference chromosome for span will be",chroms[1],"\n")
-	#pointsperchrom<-unlist(lapply(1:numchroms, function(x) nrow(curbg[which(curbg$V1==chroms[x]),])))
-
-	#})
-
-	# lscores<-mclapply(1:numchroms, function(i){
-	# 	curchrom<-curbg[which(curbg$V1==chroms[i]),]
-	# 	if(i==1){clspan=lspan} else{clspan=lspan*(pointsperchrom[i]/pointsperchrom[1])}
-	# 	goodpoints<-which(complete.cases(curchrom[,4]))
-	# 	y<-curchrom[goodpoints,4]
-	# 	x<-curchrom[goodpoints,2]
-	# 	curchrom[goodpoints,4]<-loess(y~x,span=clspan)$fitted
-	# 	curchrom
-	# },mc.cores=cores,mc.preschedule=FALSE)
-	#curbg<-curbg[order(curbg$V1,curbg$V2),]
-
 }
