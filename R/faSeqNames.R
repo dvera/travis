c3aaf2617a74af369043314ef298340c68b2f5a6
Daniel Vera <vera@genomics.fsu.edu>
2017-07-03 16:11:29 -0400

    Update faSeqNames.R

--- R/faSeqNames.R
+++ R/faSeqNames.R
@@ -2,5 +2,6 @@ faSeqNames <- function( fastaFiles, threads=getOption("threads",1L) ){
 
   cmdString <- paste0("grep \"^>\" ",fastaFiles," | sed 's/^>//g'")
   res <- cmdRun(cmdString,lines=T,threads=threads)
+  return(res)
   
 }
