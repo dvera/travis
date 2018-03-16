3902497c8dec9c9cebaecf5f54a6a9e50f4bf4eb
Daniel <dvera@bio.fsu.edu>
2018-03-16 07:07:28 -0400

    debug bedStructures

--- R/bedStructures.R
+++ R/bedStructures.R
@@ -21,13 +21,13 @@ function( bedfile , promoter5 = c(-1000,0) , promoter3 = c(0,1000) , bedname = b
 	genicname2 <- paste0(bedname,"_genic.bed")
 	intronname <- paste0(bedname,"_introns.bed")
 
-	bed<-tsvRead(bedfile)
+	bed<-read.table(bedfile,header=F,stringsAsFactors=T,sep="\t")
 
-	bed<-bed[order(bed$V1,bed$V2,bed$V3),]
+	bed<-bed[order(bed[,1],bed[,2],bed[,3]),]
 
 	if(ncol(bed) < 6){stop("no strand information found")}
-	posbed<-bed[which(bed$V6=="+"),]
-	negbed<-bed[which(bed$V6=="-"),]
+	posbed<-bed[which(bed[,6]=="+"),]
+	negbed<-bed[which(bed[,6]=="-"),]
 
 	chroms = unique(bed[,1])
 	sbed=split(bed,bed[,1])
@@ -47,26 +47,26 @@ function( bedfile , promoter5 = c(-1000,0) , promoter3 = c(0,1000) , bedname = b
 	bedSort(exonname)
 
 	utr5<-rbind(
-		data.frame(V1=posbed$V1,V2=posbed$V2,V3=posbed$V7,stringsAsFactors=F),
-		data.frame(V1=negbed$V1,V2=negbed$V8,V3=negbed$V3,stringsAsFactors=F)
+		data.frame(V1=posbed[,1],V2=posbed[,2],V3=posbed[,7],stringsAsFactors=F),
+		data.frame(V1=negbed[,1],V2=negbed[,8],V3=negbed[,3],stringsAsFactors=F)
 	)
-	utr5<-utr5[which(utr5$V3-utr5$V2 > 0),]
+	utr5<-utr5[which(utr5[,3]-utr5[,2] > 0),]
 	tsvWrite(utr5,file=utr5name)
 	bedSort(utr5name)
 
 	utr3<-rbind(
-		data.frame(V1=posbed$V1,V2=posbed$V8,V3=posbed$V3,stringsAsFactors=F),
-		data.frame(V1=negbed$V1,V2=negbed$V2,V3=negbed$V7,stringsAsFactors=F)
+		data.frame(V1=posbed[,1],V2=posbed[,8],V3=posbed[,3],stringsAsFactors=F),
+		data.frame(V1=negbed[,1],V2=negbed[,2],V3=negbed[,7],stringsAsFactors=F)
 	)
-	utr3<-utr3[which(utr3$V3-utr3$V2 > 0),]
+	utr3<-utr3[which(utr3[,3]-utr3[,2] > 0),]
 	tsvWrite(utr3,file=utr3name)
 	bedSort(utr3name)
 
 	utrname <- bedCat(c(utr5name,utr3name),utrname)
 
 	prom5<-rbind(
-		data.frame(V1=posbed$V1,V2=posbed$V2+promoter5[1],V3=posbed$V2+promoter5[2],stringsAsFactors=F),
-		data.frame(V1=negbed$V1,V2=negbed$V3-promoter5[2],V3=negbed$V3-promoter5[1],stringsAsFactors=F)
+		data.frame(V1=posbed[,1],V2=posbed[,2]+promoter5[1],V3=posbed[,2]+promoter5[2],stringsAsFactors=F),
+		data.frame(V1=negbed[,1],V2=negbed[,3]-promoter5[2],V3=negbed[,3]-promoter5[1],stringsAsFactors=F)
 	)
 	prom5[which(prom5[,2]<0),2]<-0
 	prom5=prom5[which((prom5[,3]-prom5[,2])>0),]
@@ -75,8 +75,8 @@ function( bedfile , promoter5 = c(-1000,0) , promoter3 = c(0,1000) , bedname = b
 	prom5name <- kentBedClip(prom5name,chromsizes)
 
 	prom3<-rbind(
-		data.frame(V1=posbed$V1,V2=posbed$V3+promoter3[1],V3=posbed$V3+promoter3[2],stringsAsFactors=F),
-		data.frame(V1=negbed$V1,V2=negbed$V2-promoter3[2],V3=negbed$V2-promoter3[1],stringsAsFactors=F)
+		data.frame(V1=posbed[,1],V2=posbed[,3]+promoter3[1],V3=posbed[,3]+promoter3[2],stringsAsFactors=F),
+		data.frame(V1=negbed[,1],V2=negbed[,2]-promoter3[2],V3=negbed[,2]-promoter3[1],stringsAsFactors=F)
 	)
 	prom3[which(prom3[,2]<0),2]<-0
 	prom3=prom3[which((prom3[,3]-prom3[,2])>0),]
